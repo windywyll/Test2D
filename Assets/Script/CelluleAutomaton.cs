092aeaa6696eaa54bf9c6e79ca65a132f9a53205
Windywyll <appleteensee@yahoo.fr>
2016-01-14 19:25:18 +0100

    irtnghreslghiofjklm;ù:*!

---
+++ Assets/Script/CelluleAutomaton.cs
@@ -0,0 +1,164 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CelluleAutomaton : MonoBehaviour {
+
+    public GameObject prefab;
+    Texture2D texture;
+    Color[] colorList;
+    Color[] colorListBuffer;
+    int width = 50;
+    int height = 256;
+    int iterationCount = 0;
+    float lastIterationTime = 0;
+    float iterationDelay = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	    texture = new Texture2D(width, height);
+        colorList = new Color[width * height];
+        colorListBuffer = new Color[width * height];
+        for(int i = 0; i<width*height; i++)
+        {
+            colorList[i] = Random.Range(0f, 1f) > 0.4f ? Color.white : Color.black;
+        }
+
+        texture.SetPixels(colorList);
+        texture.Apply(false);
+        texture.filterMode = FilterMode.Point;
+
+        this.GetComponent<Renderer>().material.mainTexture = texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(lastIterationTime + iterationDelay < Time.time)
+        {
+            //UpdateCubes();
+            GameOfLife();
+            lastIterationTime = Time.time;
+            iterationCount++;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                int x = (int)(hit.textureCoord.x * width);
+                int y = (int)(hit.textureCoord.y * height);
+                int index = GetIndexOfPosition(x, y);
+                if(x>=0 && x < width && y>=0 && y<height)
+                {
+                    colorList[index] = Color.white;
+                    colorListBuffer[index] = Color.white;
+                    texture.SetPixel(x, y, Color.white);
+                    texture.Apply();
+                }
+            }
+        }
+	}
+
+    bool isAlive(Color color)
+    {
+        return color == Color.white;
+    }
+
+    bool isCellALive(int index)
+    {
+        if(index >= 0 && index < width*height)
+        {
+            return isAlive(colorList[index]);
+        }
+        return false;
+    }
+
+    Vector2 GetPositionOfIndex(int index)
+    {
+        return new Vector2(index % width, index / width);
+    }
+
+    int GetIndexOfPosition(int x, int y)
+    {
+        return x + y * width;
+    }
+
+    int HowManyNeighbor(int index)
+    {
+        int neighborCount = 0;
+        Vector2 position = GetPositionOfIndex(index);
+        for (int x=-1; x<=1 ; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if(x==0 && y==0)
+                {
+                }
+                else
+                {
+                    int indexNeighbor = GetIndexOfPosition( (int)position.x + x, (int)position.y + y );
+                    if (isCellALive(indexNeighbor))
+                    {
+                        neighborCount++;
+                    }
+                }
+            }
+        }
+
+        return neighborCount;
+    }
+
+    void GameOfLife()
+    {
+        int index = 0;
+        foreach (Color col in colorList)
+        {
+            int neighborCount = HowManyNeighbor(index);
+            if(isCellALive(index))
+            {
+                if(neighborCount != 2 && neighborCount != 3)
+                {
+                    colorListBuffer[index] = Color.black;
+                }
+                else
+                {
+                    colorListBuffer[index] = Color.white;
+                }
+            }
+            else
+            {
+                if(neighborCount == 3)
+                {
+                    colorListBuffer[index] = Color.white;
+                }
+                else
+                {
+                    colorListBuffer[index] = Color.black;
+                }
+            }
+
+            index ++;
+        }
+
+        for (int i = 0; i < colorList.Length; i++)
+        {
+            colorList[i] = colorListBuffer[i];
+        }
+            
+        texture.SetPixels(colorList);
+        texture.Apply(false);
+    }
+
+    void UpdateCubes()
+    {
+        for(int i = 0; i <colorList.Length; i++){
+            if(isCellALive(i))
+            {
+                Vector2 position = GetPositionOfIndex(i);
+                GameObject.Instantiate(prefab, new Vector3(position.x, iterationCount, position.y), Quaternion.identity);
+            }
+        }
+    }
+
+}
---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float maxspeed = 4;
+    public float jumpforce = 0.1f;
+    public float gravity = 1;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    [HideInInspector]
+    public bool lookingRight = true;
+
+    private Rigidbody2D rb2d;
+    private bool isGrounded = false;
+    // Use this for initialization
+    void Awake()
+    {
+        groundCheck = transform.Find("GroundCheck");
+    }
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector2(hor * maxspeed, rb2d.velocity.y);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpforce * maxspeed);
+        }
+
+        if(!isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y - (gravity * Time.deltaTime));
+        }
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15F, whatIsGround);
+
+        if ((hor > 0 && !lookingRight) || (hor < 0 && lookingRight))
+            Flip();
+
+
+    }
+
+    public void Flip()
+    {
+        lookingRight = !lookingRight;
+        Vector3 myScale = transform.localScale;
+        myScale.x *= -1;
+        transform.localScale = myScale;
+    }
+} 
